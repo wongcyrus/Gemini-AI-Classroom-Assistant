87939e7a4441cf738843c0bc69d08e8af37f200c
cy.gdoc <cy.gdoc@gmail.com>
2025-10-05 04:15:43 +0000

    rename to camelCase

--- functions/index.mjs
+++ functions/index.mjs
@@ -1,16 +1,16 @@
 import './firebase.js';
 
 import { onCallGenkit } from "firebase-functions/v2/https";
-import { analyzeImageFlow, analyzeAllImagesFlow } from "./analysis-flows.js";
+import { analyzeImageFlow, analyzeAllImagesFlow } from "./analysisFlows.js";
 
 export * from './processVideoJob.js';
 export * from './processZipJob.js';
 export * from './storageQuota.js';
 export * from './ipRestriction.js';
-export * from './screenshot-management.js';
+export * from './screenshotManagement.js';
 export * from './cleanupStuckJobs.js';
 export * from './processVideoAnalysisJob.js';
-export * from './quota-triggers.js';
+export * from './quotaTriggers.js';
 export * from './scheduledTasks.js';
 export * from './userManagement.js';
 import { CORS_ORIGINS } from './config.js';
